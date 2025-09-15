0fc6e95b25810356e3bd1476e1d8a97dc6932ac9
shubro07 <136472052+shubro007@users.noreply.github.com>
2025-09-15 23:58:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+//app.use(express.json());
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+  console.log('A user connected with ID:', socket.id);
+  socket.on('disconnect', () => {
+    console.log(socket.id, ' user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile('E:/coding challenges/sockets/index.html');
+});
+
+httpServer.listen(3000, (res,req) => {
+    console.log("Server is Running");
+});
+
+// app.listen(3000, (res, req) => {
+//     console.log("hi");
+// })
+
+
+
