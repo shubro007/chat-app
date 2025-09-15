04b3f67ccdb44389de61d2ba1e9579f8115d80ec
shubro07 <136472052+shubro007@users.noreply.github.com>
2025-09-16 01:50:40 +0530

    added support for nicknames

--- app.js
+++ app.js
@@ -1,36 +1,47 @@
 import express from 'express';
-
 import { createServer } from "http";
 import { Server } from "socket.io";
+import path from 'path';
 
 const app = express();
-//app.use(express.json());
 const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
-
-io.on("connection", (socket) => {
-  io.emit('chat message',`${socket.id} connected to chat`);
-  console.log('A user connected with ID:', socket.id);
-  socket.on('disconnect', () => {
-    console.log(socket.id, ' user disconnected');
-    io.emit('chat message',`${socket.id} disconnected from chat`);
-  });
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-});
+const __dirname = path.resolve();
 
 app.get('/', (req, res) => {
-    res.sendFile('E:/coding challenges/sockets/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
-httpServer.listen(3000, (res,req) => {
-    console.log("Server is Running");
+// Main connection logic
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  // 1. Listen for the new 'nick name' event
+  socket.on('nick name', (nickname) => {
+    // Store the nickname on the socket object for this user
+    socket.nickname = nickname;
+    console.log(`${nickname} has joined the chat.`);
+    
+    // Broadcast to everyone (except the sender) that a new user joined
+    socket.broadcast.emit('chat message', `${nickname} has joined the chat.`);
+  });
+
+  // 2. Listen for the 'chat message' event
+  socket.on('chat message', (msg) => {
+    // Prepend the user's stored nickname to the message
+    // Then broadcast it to EVERYONE (including the sender)
+    io.emit('chat message', `${socket.nickname}: ${msg}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+    if (socket.nickname) {
+      // Announce that the user left
+      io.emit('chat message', `${socket.nickname} has left the chat.`);
+    }
+  });
 });
 
-// app.listen(3000, (res, req) => {
-//     console.log("hi");
-// })
-
-
-
+httpServer.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
